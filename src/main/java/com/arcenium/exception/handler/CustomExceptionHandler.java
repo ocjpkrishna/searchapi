00fb52187cae7f5f59b5e9322c06118ac289386e
Krishna <krishnakumaryadavk@airasia.com>
2023-10-29 05:17:56 +0530

    arc- added globle exception handler

---
+++ src/main/java/com/arcenium/exception/handler/CustomExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.arcenium.exception.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {}
