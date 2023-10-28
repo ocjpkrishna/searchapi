8b632dfa747e76ffb39a723ba21e9c398f440ce0
Krishna <krishnakumaryadavk@airasia.com>
2023-10-29 05:27:58 +0530

    arc- updated service for user defined exception

--- src/main/java/com/arcenium/service/ContactSearchService.java
+++ src/main/java/com/arcenium/service/ContactSearchService.java
@@ -1,6 +1,7 @@
 package com.arcenium.service;
 
 import com.arcenium.domain.User;
+import com.arcenium.exception.UserExceedException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -32,8 +33,8 @@ public class ContactSearchService {
     }
 
     public List<Integer> apiResponse(String field, String operation, List<String> values) {
-        if(values.size() > 3){
-            throw  new RuntimeException("more than three User not allowed");
+        if (values.size() > 3) {
+            throw new UserExceedException("more than three User not allowed");
         }
         List<Integer> result = new ArrayList<>();
         for (String value : values) {
@@ -46,37 +47,37 @@ public class ContactSearchService {
         return result;
     }
 
-        private boolean matchesCriteria(User user, String fieldName, String operationType, String value) {
-            try {
-                String[] fieldParts = fieldName.split("\\.");
+    private boolean matchesCriteria(User user, String fieldName, String operationType, String value) {
+        try {
+            String[] fieldParts = fieldName.split("\\.");
 
-                Object currentObject = user;
-                for (String part : fieldParts) {
-                    Field field = currentObject.getClass().getDeclaredField(part);
-                    field.setAccessible(true);
-                    currentObject = field.get(currentObject);
-                }
+            Object currentObject = user;
+            for (String part : fieldParts) {
+                Field field = currentObject.getClass().getDeclaredField(part);
+                field.setAccessible(true);
+                currentObject = field.get(currentObject);
+            }
 
-                if (currentObject == null) {
-                    return false;
-                }
-
-                String stringValue = currentObject.toString();
-
-                switch (operationType) {
-                    case "Equals":
-                        return stringValue.equals(value);
-                    case "IN":
-                        List<String> values = Arrays.asList(value.split(","));
-                        return values.contains(stringValue);
-                    default:
-                        throw new IllegalArgumentException("Unsupported operation type: " + operationType);
-                }
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                e.printStackTrace();
+            if (currentObject == null) {
                 return false;
             }
+
+            String stringValue = currentObject.toString();
+
+            switch (operationType) {
+                case "Equals":
+                    return stringValue.equals(value);
+                case "IN":
+                    List<String> values = Arrays.asList(value.split(","));
+                    return values.contains(stringValue);
+                default:
+                    throw new IllegalArgumentException("Unsupported operation type: " + operationType);
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+            return false;
         }
+    }
 
     private List<User> fetchUserData() {
         try {
@@ -94,7 +95,7 @@ public class ContactSearchService {
                     }
                     final List<User> users1 = objectMapper.readValue(content.toString(), new TypeReference<List<User>>() {
                     });
-                    log.info("all users from Endpoint: {} ", users1 );
+                    log.info("all users from Endpoint: {} ", users1);
                     return users1;
                 }
             } else {
